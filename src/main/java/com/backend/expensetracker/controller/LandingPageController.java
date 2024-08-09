2ca6392c858f75795ffa5a03488821a3885380da
Chitin1627 <pratyushprashob@gmail.com>
2024-08-09 17:06:05 +0530

    small landing page

---
+++ src/main/java/com/backend/expensetracker/controller/LandingPageController.java
@@ -0,0 +1,14 @@
+package com.backend.expensetracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class LandingPageController {
+    @GetMapping("")
+    String landingPage() {
+        return "Expense Tracker App";
+    }
+}
