c03add00a0370c8f14596cbe57b04cad4636d27f
Chitin1627 <pratyushprashob@gmail.com>
2024-09-02 00:15:29 +0530

    Fixed a bug 2

--- src/main/java/com/backend/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/backend/expensetracker/controller/ExpenseController.java
@@ -161,23 +161,19 @@ public class ExpenseController {
         String username = authentication.getName();
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
-        calendar.add(Calendar.DAY_OF_YEAR, -1);
         Date startDate = calendar.getTime();
 
         calendar.setTime(date);
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         calendar.add(Calendar.DAY_OF_YEAR, 1);
         Date endDate = calendar.getTime();
-
         return expenseRepository.findByUsernameAndDateBetween(username, startDate, endDate);
     }
 
